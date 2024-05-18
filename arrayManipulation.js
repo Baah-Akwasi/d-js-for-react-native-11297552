068e0f579724acefe04328b71a775185599bc693
Akwasi <baahakwasi4009@gmail.com>
2024-05-18 12:58:19 +0000

    This is the code for the second task

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -9,3 +9,16 @@ function processArray(parameter) {
     }
   });
 }
+
+const formatArrayStrings = function (strings, numbers) {
+  if (strings.length !== numbers.length) {
+    prompt("They should be of the same lenght");
+  }
+  return strings.map((str, i) => {
+    if (numbers[i] % 2 === 0) {
+      return str.toUpperCase();
+    } else {
+      return str.toLowerCase();
+    }
+  });
+};
